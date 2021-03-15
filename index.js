5560567250fa90a17f5428cba66379a964840266
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-03-15 22:02:40 +0200

    get img from my  instagram

--- index.js
+++ index.js
@@ -3,41 +3,43 @@ const puppeteer = require('puppeteer');
 
 //get instagram img todo
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless:false});
   const page = await browser.newPage();
   await page.goto('https://www.instagram.com/agostinho_dos_santos_machava/');
 
 
-  await page.evaluate (()=> {
-      const nodeList = document.querySelectorAll("article img");
+  const imgList = await page.evaluate (()=> {
+      const nodeList = document.querySelectorAll('article img');
       const imgArray = [...nodeList];
 
-      const list = imgArray.map( ({src}) => {
+      const imgList = imgArray.map( ({src}) => ({
           src
-      })
+      }))
 
-      console.log(list);
+      return imgList;
   })
+
+  console.log(imgList);
 
   await browser.close();
 })();
 
 
 // get wikipedia head
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://pt.wikipedia.org/wiki/Coronav%C3%ADrus');
+// (async () => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto('https://pt.wikipedia.org/wiki/Coronav%C3%ADrus');
 
-    const result = await page.evaluate(() => {
-        let headingFroWeb = document.querySelectorAll(".mw-headline");
-        const headingList = [...headingFroWeb];
-        return headingList.map(h => h.innerText);
-    })
+//     const result = await page.evaluate(() => {
+//         let headingFroWeb = document.querySelectorAll(".mw-headline");
+//         const headingList = [...headingFroWeb];
+//         return headingList.map(h => h.innerText);
+//     })
 
-    console.log(result);
+//     console.log(result);
   
 
-    await browser.close();
-  })();
+//     await browser.close();
+//   })();
   
