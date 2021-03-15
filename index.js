b18150415bb6d47251fd4ecb16381acab7c47020
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-03-15 22:26:15 +0200

    create json with my instagram images

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 
 //get instagram img todo
@@ -19,27 +20,34 @@ const puppeteer = require('puppeteer');
       return imgList;
   })
 
+  // write data local
+  fs.writeFile('instagram.json' , JSON.stringify(imgList, null , 2), err => {
+      if(err) throw new Error("something went wrong");
+
+      console.log("well done");
+  })
+
   console.log(imgList);
 
   await browser.close();
 })();
 
 
-// get wikipedia head
-// (async () => {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     await page.goto('https://pt.wikipedia.org/wiki/Coronav%C3%ADrus');
+//get wikipedia head
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://pt.wikipedia.org/wiki/Coronav%C3%ADrus');
 
-//     const result = await page.evaluate(() => {
-//         let headingFroWeb = document.querySelectorAll(".mw-headline");
-//         const headingList = [...headingFroWeb];
-//         return headingList.map(h => h.innerText);
-//     })
+    const result = await page.evaluate(() => {
+        let headingFroWeb = document.querySelectorAll(".mw-headline");
+        const headingList = [...headingFroWeb];
+        return headingList.map(h => h.innerText);
+    })
 
-//     console.log(result);
+    console.log(result);
   
 
-//     await browser.close();
-//   })();
+    await browser.close();
+  })();
   
